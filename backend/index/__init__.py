f5a0989d44d5b422bf0d6bba94de43e9b9e827da
Maria <mariaibanez@delosintelligence.fr>
2025-02-03 17:19:06 +0100

    small fix

--- backend/index/__init__.py
+++ backend/index/__init__.py
@@ -1,7 +1,6 @@
 """app and routers of the application."""
 
-from .models import IndexCreateModel
 from .server import app, start
 from .utils import logger
 
-__all__ = ["app", "start", "logger", "IndexCreateModel"]
+__all__ = ["app", "start", "logger"]
