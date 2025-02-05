f828a3ee4761a2e24a583bea84b076921ba6dd0e
Maria <mariaibanez@delosintelligence.fr>
2025-02-05 11:35:15 +0100

    improve logging

--- backend/index/routes.py
+++ backend/index/routes.py
@@ -2,10 +2,11 @@ from io import BytesIO
 from typing import IO, Any
 
 from cosmos import CosmosClient
-from fastapi import APIRouter, Depends, Form, HTTPException, UploadFile
+from fastapi import APIRouter, Form, HTTPException, UploadFile
 
 from .config import settings
 from .errors import ClientInitializationError, SettingsError
+from .utils import logger
 
 router = APIRouter()
 
@@ -14,31 +15,33 @@ def get_client() -> CosmosClient:
     try:
         if not hasattr(settings, "COSMOS_APIKEY") or not settings.COSMOS_APIKEY:
             raise SettingsError("COSMOS_APIKEY is missing or empty.")
-        print(f"key is `{settings.COSMOS_APIKEY}`, server is `{settings.API_SERVER}`")
+        logger.debug(f"Initializing client: key is `{settings.COSMOS_APIKEY}`, server is `{settings.API_SERVER}`")
 
         client = CosmosClient(settings.COSMOS_APIKEY, server_url=settings.API_SERVER)
-        print(client.status_health_request())
+        logger.debug(f"Test API health status: {client.status_health_request()}")
 
         if not client:
             raise ClientInitializationError("Failed to initialize the client.")
-        # print(f"client is {client}")
         return client
 
     except SettingsError as e:
-        print(f"Error: {e}")
+        logger.debug(f"Error: {e}")
         raise
 
     except ClientInitializationError as e:
-        print(f"Error: {e}")
+        logger.debug(f"Error: {e}")
         raise
 
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        logger.debug(f"An unexpected error occurred: {e}")
         raise
 
 
+client = get_client()
+
+
 @router.get("/files/index/list")
-async def list_indexes(client: CosmosClient = Depends(get_client)) -> dict[str, Any] | None:
+async def list_indexes() -> dict[str, Any] | None:
     try:
         return client.files_index_list_request()
     except Exception as e:
@@ -46,21 +49,19 @@ async def list_indexes(client: CosmosClient = Depends(get_client)) -> dict[str, 
 
 
 @router.get("/files/index/{index_uuid}/details")
-async def index_details(index_uuid: str, client: CosmosClient = Depends(get_client)) -> dict[str, Any] | None:
+async def index_details(index_uuid: str) -> dict[str, Any] | None:
     try:
-        print(f"Received parameters: index_uuid={index_uuid}")
+        logger.debug(f"Received parameters: index_uuid={index_uuid}")
         return client.files_index_details_request(index_uuid=index_uuid)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 @router.post("/files/index/create")
-async def create_index(
-    name: str = Form(...), filesobjects: list[UploadFile] = Form(...), client: CosmosClient = Depends(get_client)
-) -> dict[str, Any] | None:
+async def create_index(name: str = Form(...), filesobjects: list[UploadFile] = Form(...)) -> dict[str, Any] | None:
     try:
-        print(f"Received name: {name}")
-        print(f"Received files: {[file.filename or '' for file in filesobjects]}")
+        logger.debug(f"Received name: {name}")
+        logger.debug(f"Received files: {[file.filename or '' for file in filesobjects]}")
 
         # Convert the uploaded files to the format expected by the client
         files_data: list[tuple[str, tuple[str, IO[bytes]]]] = []
@@ -72,7 +73,7 @@ async def create_index(
         return new_index
 
     except Exception as e:
-        print(f"Exception : {e}")
+        logger.debug(f"Exception : {e}")
         raise HTTPException(status_code=400, detail=str(e))
 
 
@@ -82,10 +83,9 @@ async def ask_index(
     question: str,
     output_language: str,
     active_files_hashes: list[str],
-    client: CosmosClient = Depends(get_client),
 ) -> dict[str, Any] | None:
     try:
-        print(
+        logger.debug(
             f"Received parameters: index_uuid={index_uuid}, question={question}, output_language={output_language}, active_files_hashes={active_files_hashes}"
         )
         return client.files_index_ask_request(
@@ -99,21 +99,19 @@ async def ask_index(
 
 
 @router.post("/files/index/{index_uuid}/embed")
-async def embed_index(index_uuid: str, client: CosmosClient = Depends(get_client)) -> dict[str, Any] | None:
+async def embed_index(index_uuid: str) -> dict[str, Any] | None:
     try:
-        print(f"Received parameters: index_uuid={index_uuid}")
+        logger.debug(f"Received parameters: index_uuid={index_uuid}")
         return client.files_index_embed_request(index_uuid=index_uuid)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 @router.post("/files/index/{index_uuid}/add_files")
-async def add_files_to_index(
-    index_uuid: str, filesobjects: list[UploadFile] = Form(...), client: CosmosClient = Depends(get_client)
-) -> dict[str, Any] | None:
+async def add_files_to_index(index_uuid: str, filesobjects: list[UploadFile] = Form(...)) -> dict[str, Any] | None:
     try:
-        print(f"Received parameters: index_uuid={index_uuid}")
-        print(f"Received files: {[file.filename for file in filesobjects]}")
+        logger.debug(f"Received parameters: index_uuid={index_uuid}")
+        logger.debug(f"Received files: {[file.filename for file in filesobjects]}")
 
         # Convert the uploaded files to the format expected by the client
         files_data: list[tuple[str, tuple[str, IO[bytes]]]] = []
@@ -127,41 +125,39 @@ async def add_files_to_index(
 
 
 @router.delete("/files/index/{index_uuid}/delete_files")
-async def delete_files_from_index(
-    index_uuid: str, files_hashes: list[str], client: CosmosClient = Depends(get_client)
-) -> dict[str, Any] | None:
-    print(f"Received parameters: index_uuid={index_uuid}, files_hashes={files_hashes} type {type(files_hashes)}")
+async def delete_files_from_index(index_uuid: str, files_hashes: list[str]) -> dict[str, Any] | None:
+    logger.debug(f"Received parameters: index_uuid={index_uuid}, files_hashes={files_hashes} type {type(files_hashes)}")
     try:
-        print(f"Received parameters: index_uuid={index_uuid}, files_hashes={files_hashes}")
+        logger.debug(f"Received parameters: index_uuid={index_uuid}, files_hashes={files_hashes}")
         response = client.files_index_delete_files_request(index_uuid=index_uuid, files_hashes=files_hashes)
-        print(f"Response: {response}")
+        logger.debug(f"Response: {response}")
         return response
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 @router.delete("/files/index/{index_uuid}/delete")
-async def delete_index(index_uuid: str, client: CosmosClient = Depends(get_client)):
+async def delete_index(index_uuid: str):
     try:
-        print(f"Received parameters: index_uuid={index_uuid}")
+        logger.debug(f"Received parameters: index_uuid={index_uuid}")
         return client.files_index_delete_request(index_uuid=index_uuid)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 @router.put("/files/index/{index_uuid}/restore")
-async def restore_index(index_uuid: str, client: CosmosClient = Depends(get_client)):
+async def restore_index(index_uuid: str):
     try:
-        print(f"Received parameters: index_uuid={index_uuid}")
+        logger.debug(f"Received parameters: index_uuid={index_uuid}")
         return client.files_index_restore_request(index_uuid=index_uuid)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
 @router.put("/files/index/{index_uuid}/rename")
-async def rename_index(index_uuid: str, name: str, client: CosmosClient = Depends(get_client)):
+async def rename_index(index_uuid: str, name: str):
     try:
-        print(f"Received parameters: index_uuid={index_uuid}, name={name}")
+        logger.debug(f"Received parameters: index_uuid={index_uuid}, name={name}")
         return client.files_index_rename_request(index_uuid=index_uuid, name=name)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
